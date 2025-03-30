218b5f6c6f7665cfdfda9584d68eda0368c833cf
Charles Django <charlesdc12446@gmail.com>
2025-03-30 13:28:34 +0100

    Setup asset library and started wall running paths.

--- The Flipper Framework/Assets/!Framework/Scripts/Objects/Addons/S_RotateObject.cs
+++ The Flipper Framework/Assets/!Framework/Scripts/Objects/Addons/S_RotateObject.cs
@@ -1,26 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class S_RotateObject : MonoBehaviour {
+public class S_RotateObject : MonoBehaviour
+{
 
-    public Vector3 RotateSpeed;
-    public float InitialRandomRotationAmm;
-    public bool SetBasedOnXpos = true;
+	public Vector3 RotateSpeed;
+	public float InitialRandomRotationAmm;
+	public bool SetBasedOnXpos = true;
 
-    void Start()
-    {
-        Vector3 rand = new Vector3(0, Random.Range(0, InitialRandomRotationAmm), 0);
-        transform.Rotate(rand);
-        if (SetBasedOnXpos)
-        {
-            Vector3 set = new Vector3(0, transform.position.x + transform.position.z, 0);
-            transform.Rotate(set);
-        }
-    }
+	void Start () {
+		Vector3 rand = new Vector3(0, Random.Range(0, InitialRandomRotationAmm), 0);
+		transform.Rotate(rand);
+		if (SetBasedOnXpos)
+		{
+			Vector3 set = new Vector3(0, transform.position.x + transform.position.z, 0);
+			transform.Rotate(set);
+		}
+	}
 
 	void Update () {
 
-        transform.Rotate(RotateSpeed * Time.deltaTime);
+		transform.Rotate(RotateSpeed * Time.deltaTime);
 
-    }
+	}
 }
